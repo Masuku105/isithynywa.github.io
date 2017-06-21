a2cc586441f0d778c42d104dbcf81c25684e1cd1
Masuku105 <masuku105@gmail.com>
2017-06-21 21:57:01 +0200

    Asambe

---
+++ animations/animation.js
@@ -0,0 +1,152 @@
+//Animation For About Section
+$(window).scroll(function() {
+	$('#about .container').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+400) {
+			$(this).addClass("fadeIn");
+		}
+	});
+});
+
+
+//Animation For Web Promotional Section
+$(window).scroll(function() {
+	$('#web_promotional_1 .container').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+400) {
+			$(this).addClass("fadeIn");
+		}
+	});
+});
+
+
+//Animation For Features Section
+$(window).scroll(function() {
+	$('#features .container').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+400) {
+			$(this).addClass("fadeIn");
+		}
+	});
+});
+
+
+//Animation For Services Section
+$(window).scroll(function() {
+	$('#services .container').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+400) {
+			$(this).addClass("fadeIn");
+		}
+	});
+});
+
+//Animation For Dentist Services Section
+$(window).scroll(function() {
+	$('#dentist_services .container').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+400) {
+			$(this).addClass("fadeIn");
+		}
+	});
+});
+
+//Animation For Pet Clinic Services Section
+$(window).scroll(function() {
+	$('#pet_services .container').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+400) {
+			$(this).addClass("fadeIn");
+		}
+	});
+});
+
+
+//Animation For Why Choose Us Section
+$(window).scroll(function() {
+	$('#why_choose .container').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+400) {
+			$(this).addClass("fadeIn");
+		}
+	});
+});
+
+
+//Animation For Time Table Section
+$(window).scroll(function() {
+	$('#time_table .container').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+400) {
+			$(this).addClass("fadeIn");
+		}
+	});
+});
+
+
+//Animation For Price Table Section
+$(window).scroll(function() {
+	$('#price_tbl .container').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+400) {
+			$(this).addClass("fadeIn");
+		}
+	});
+});
+
+
+//Animation For Team Section
+$(window).scroll(function() {
+	$('#team .container').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+400) {
+			$(this).addClass("fadeIn");
+		}
+	});
+});
+
+
+//Animation For Testimonial
+$(window).scroll(function() {
+	$('#testimonial .container').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+400) {
+			$(this).addClass("fadeIn");
+		}
+	});
+});
+
+
+//Animation For Contact
+$(window).scroll(function() {
+	$('#contact .container').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+400) {
+			$(this).addClass("fadeIn");
+		}
+	});
+});
